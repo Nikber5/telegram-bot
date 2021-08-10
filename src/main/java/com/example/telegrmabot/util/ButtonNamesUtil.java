47ec7136093b8a191fe9f50970f4e3afc9e1d5a8
Nikber5 <nikber198@gmail.com>
2021-08-10 13:03:09 +0300

    encrypted youtube response message

--- src/main/java/com/example/telegrmabot/util/ButtonNamesUtil.java
+++ src/main/java/com/example/telegrmabot/util/ButtonNamesUtil.java
@@ -6,7 +6,7 @@ import java.util.List;
 public final class ButtonNamesUtil {
     public static final String SEND_LINK = "Send my link";
     public static final String YOUTUBE_REQUEST = "Find video in youtube";
-    public static final String YOUTUBE_RESPONSE = "Youtube Response";
+    public static final String YOUTUBE_RESPONSE = "*#&&!!";
     public static final String DEFAULT = "DefaultButton";
 
     private ButtonNamesUtil() {
